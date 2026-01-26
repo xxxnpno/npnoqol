456868a9b65ea7dd8deb23839ad9ab2b1b969f52
xxxnpno <arnolegrin@outlook.fr>
2026-01-26 21:26:00 +0100

    added ChatManager

--- npno/src/module/hypixel/all/ChatManager/ChatManager.cpp
+++ npno/src/module/hypixel/all/ChatManager/ChatManager.cpp
@@ -11,34 +11,36 @@ hypixel::ChatManager::ChatManager()
 
 hypixel::ChatManager::~ChatManager() = default;
 
-auto hypixel::GamemodeManager::Update() -> void
+auto hypixel::ChatManager::Update() -> void 
 {
     const std::vector<std::unique_ptr<ChatLine>>& chatLines = mc->GetIngameGUI()->GetPersistantChatGUI()->GetChatLines();
 
     bool refresh = false;
-    if (!chatLines[0]->GetLineString()->GetFormattedText().contains(":"))
+    if (!chatLines[0]->GetLineString()->GetFormattedText().contains(":")) 
     {
-        for (const std::string& line : this->blacklistedLines)
+        for (const std::string& line : this->blacklistedLines) 
         {
-            if (text.find(line) != std::string::npos)
+            if (chatLines[0]->GetLineString()->GetFormattedText().find(line) != std::string::npos) 
             {
-                mc->GetIngameGUI()->GetPersistantChatGUI()->DeleteChatLine(chatLines[0]->GetLineString()->GetChatLineID());
+                mc->GetIngameGUI()->GetPersistantChatGUI()->DeleteChatLine(chatLines[0]->GetChatLineID());
+                refresh = true; 
+                break; 
             }
         }
     }
 
-    if (refresh)
+    if (refresh) 
     {
         mc->GetIngameGUI()->GetPersistantChatGUI()->RefreshChat();
     }
 }
 
-auto hypixel::GamemodeManager::AddBlacklistedLine(const std::string& line) -> void
+auto hypixel::ChatManager::AddBlacklistedLine(const std::string& line) -> void
 {
     this->blacklistedLines.push_back(line);
 }
 
-auto hypixel::GamemodeManager::AddBlacklistedLines(const std::vector<std::string>& lines) -> void
+auto hypixel::ChatManager::AddBlacklistedLines(const std::vector<std::string>& lines) -> void
 {
     this->blacklistedLines.insert(this->blacklistedLines.end(), std::make_move_iterator(lines.begin()), std::make_move_iterator(lines.end()));
 }
